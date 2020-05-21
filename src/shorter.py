16d6ac68ff86ddd395a72debe2151c78ee40a50d
Kaio Cesar <tecnico.kaio@gmail.com>
2020-05-20 23:02:09 -0300

    first method with green pass

--- src/shorter.py
+++ src/shorter.py
@@ -1,8 +1,11 @@
 
 class Shorter:
-    def __init__(self, code):
+    def __init__(self, code='', url=''):
         self.code = code
-        self.url = ''
+        self.url = url
 
     def get_url(self):
-        return 'ok'
+        return self.url
+
+    def get_code(self):
+        return 'not a null'
